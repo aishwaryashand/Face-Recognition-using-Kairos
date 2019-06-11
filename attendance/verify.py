1c1830a676416b819ca7f83809de3d54743c5301
Aishwarya Shand <aishwarya14696@gmail.com>
2019-06-11 15:25:57 +0530

    Update verify.py

--- attendance/verify.py
+++ attendance/verify.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python2
 from urllib2 import Request, urlopen
 
+#!/usr/bin/python2
+import cv2
+from urllib2 import Request, urlopen
+import base64
+import json
+import camera
+
+encoded_string = base64.b64encode(open("Gallery/test.jpg", 'r').read())
+payload_dict = {
+    "image":encoded_string,
+    "selector": "ROLL"
+}
+payload = json.dumps(payload_dict)
+
+headers = {
+  'Content-Type': 'application/json',
+  "app_id": "01646bc9",
+  "app_key": "981a8af72b5b9e19a7d29e3938c70749"
+}
+
 values = """
   {
     "image": "http://18.222.253.203/images/subject_id.jpg",
     "gallery_name": "MyGallery",
-    "subject_id": "Aishwarya Shand"
+    "subject_id": "kamakshi"
     
   }
 """
 
 headers = {
   'Content-Type': 'application/json',
-  "app_id": "437f528e",
-  "app_key": "79b5ee9c50d854b4647a916a121891ea"
+  "app_id": "01646bc9",
+  "app_key": "981a8af72b5b9e19a7d29e3938c70749"
 }
 request = Request('https://api.kairos.com/verify', data=values, headers=headers)
 
 response_body = urlopen(request).read()
 print response_body
-
 
 
